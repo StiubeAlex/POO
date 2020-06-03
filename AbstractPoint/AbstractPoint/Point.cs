8dc558bafbc9a0ca194d89086d04faa26d215fb7
Știube Alexandru <56305912+StiubeAlex@users.noreply.github.com>
2020-06-03 15:51:05 +0300

    Tuesday, 2 June 2020, 9:41 PM

---
+++ AbstractPoint/AbstractPoint/Point.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractPoint
+{
+    public class Point : AbstractPoint
+    {
+        private float aa;
+        private float bb;
+        private float rr;
+        private float AA;
+        public override float a
+        {
+            get { return aa; }
+            set { aa = value; rr = rGet(); AA = AGet(); }
+        }
+        public override float b
+        {
+            get { return bb; }
+            set { bb = value; rr = rGet(); AA = AGet(); }
+        }
+        public override float r
+        {
+            get { return rr; }
+            set { rr = value; aa = aGet(); bb = bGet(); }
+        }
+        public override float A
+        {
+            get { return AA; }
+            set { AA = value; aa = aGet(); bb = bGet(); }
+        }
+        public Point(PointRepresentation pr, float ar, float bA)
+        {
+            if (pr == PointRepresentation.Polar)
+            {
+                rr = ar;
+                AA = bA;
+                aa = aGet();
+                bb = bGet();
+            }
+            if (pr == PointRepresentation.Rectangular)
+            {
+                aa = ar;
+                bb = bA;
+                rr = rGet();
+                AA = AGet();
+            }
+        }
+        public override void Move(float x, float y)
+        {
+            aa += x;
+            bb += y;
+            rr = rGet();
+            AA = AGet();
+        }
+        public override void Rotate(float angle)
+        {
+            AA += angle;
+            aa = aGet();
+            bb = bGet();
+        }
+    }
+}
---
+++ AbstractPoint/AbstractPoint/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(PointRepresentation.Rectangular, 2, 2);
+            Point p2 = new Point(PointRepresentation.Polar, 5, 2);
+            p1.Move(5,5);
+            p1.Rotate(2);
+            p2.Move(0, -5);
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+            Console.ReadKey();
+        }
+    }
+}
