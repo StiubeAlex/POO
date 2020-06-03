784beb40eb17de49ffa6285646e82fff95740537
Știube Alexandru <56305912+StiubeAlex@users.noreply.github.com>
2020-06-03 15:52:18 +0300

    Tuesday, 2 June 2020, 9:09 PM

---
+++ Agenda/Agenda Personala/Persoana.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda_Personala
+{
+    public class Person
+    {
+        private string numeFam;
+        private string prenume;
+        public DateTime dataNasterii;
+        public string adresaEmail;
+        public Agenda notebook;
+        public string Nume
+        {
+            get { return numeFam; }
+            set { numeFam = value; }
+        }
+        public string Prenume
+        {
+            get { return prenume; }
+            set { prenume = value; }
+        }
+        public DateTime DataNasterii
+        {
+            get { return dataNasterii; }
+            set { dataNasterii = value; }
+        }
+        public string CreeazaNume() { return $"{this.numeFam} {this.prenume}"; }
+        public string CreeazaDate() { return $"{CreeazaNume()}, {this.DataNasterii.ToShortDateString()}, {adresaEmail}"; }
+    }
+}
---
+++ Agenda/Agenda Personala/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda_Personala
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person tesla = Engine.CreatePerson("Nikola", "Tesla", new DateTime(1856, 7, 10), "tesla_nikola@yahoo.com");
+            Person turing = Engine.CreatePerson("Alan", "Turing", new DateTime(1912, 6, 23), "turing.al@gmail.com");
+            Console.WriteLine();
+            Activity plimbari = tesla.CreateActivity("plimbari", "(prin parc)", DateTime.Now, DateTime.Now.AddHours(1));
+            Activity hobby = Engine.CreateActivity("sport", "(alergare)", DateTime.Now, DateTime.Now.AddHours(3));
+            Console.WriteLine();
+            Console.WriteLine();
+            turing.AddActivity(plimbari);
+            turing.AddActivity(hobby);
+            Console.WriteLine();
+            List<Activity> allactivities = tesla.FindActivitiesByName("hobby");
+            turing.FindActivitiesByInterval(new DateTime(2020, 4, 23), new DateTime(2020, 4, 24));
+            Console.WriteLine();
+            Engine.DeleteActivity(plimbari);
+            Console.WriteLine();
+            Engine.FindMeetingWithGroup(2, tesla, turing);
+            Console.ReadLine();
+        }
+    }
+}
