7446546d0e469d1f2f1d32ef41b608ad46646a34
Știube Alexandru <56305912+StiubeAlex@users.noreply.github.com>
2020-06-07 10:53:51 +0300

    Add files via upload

---
+++ Form 2D/Form 2D/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Form_2D
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Cerc cerc = new Cerc(4);
+			Patrat patrat = new Patrat(7);
+			Print(cerc);
+			Print(patrat);
+			Console.ReadKey();
+		}
+		public static void Print(Form form)
+		{
+			Console.WriteLine("Forma: " + form.Forma);
+			Console.WriteLine("Arie: " + form.Arie());
+			Console.WriteLine("Perimetru: " + form.Perimetru());
+			Console.WriteLine();
+		}
+	}
+}
---
+++ Form 2D/Form 2D/Tools.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Form_2D
+{
+    public interface Form
+    {
+        string Forma { get; }
+        float Arie();
+        float Perimetru();
+    }
+    public class Cerc : Form
+    {
+        public string Forma => "Cerc";
+        public float radius;
+        public Cerc(float radius) { this.radius = radius; }
+        public float Arie() { return (float)Math.PI * radius * radius; }
+        public float Perimetru() { return 2f * (float)Math.PI * radius; }
+    }
+    public class Patrat : Form
+    {
+        public string Forma => "Patrat";
+        public float length;
+        public Patrat(float length) { this.length = length; }
+        public float Arie() { return length * length; }
+        public float Perimetru() { return 4f * length; }
+    }
+}
