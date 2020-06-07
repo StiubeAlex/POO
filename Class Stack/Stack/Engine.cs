6995468e232b4d05dce4c5849b43760364c33cff
Știube Alexandru <56305912+StiubeAlex@users.noreply.github.com>
2020-06-07 20:50:48 +0300

    Add files via upload

---
+++ Class Stack/Stack/Engine.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class Stack<stiva>
+    {
+        public stiva[] items;
+        private int index;
+
+        public int Ct
+        {
+            get { return index >= 0 ? index + 1 : 0; }
+            set { index = value; }
+        }
+
+        public Stack(int cantity)
+        {
+            index = -1;
+            items = new stiva[cantity];
+        }
+
+        public void inn(stiva item)
+        {
+            if (index + 1 >= items.Length)
+            {
+                Console.WriteLine("Stack ul este plin");
+            }
+            else
+            {
+                index++;
+                items[index] = item;
+            }
+        }
+
+        public stiva ok()
+        {
+            if (index < 0) { Console.WriteLine("Nu mai avem ce sa scoatem din Stack"); return default; }
+
+            stiva value = items[index];
+            items[index] = default;
+            index--;
+            return value;
+        }
+
+        public void Clear()
+        {
+            index = 0;
+            items = new stiva[items.Length];
+        }
+    }
+}
---
+++ Class Stack/Stack/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            Stack<int> ints = new Stack<int>(3);
+            ints.inn(5);
+            Console.WriteLine(ints.ok());
+            ints.ok();
+            ints.inn(2);
+            ints.inn(1);
+            Console.WriteLine();
+            Stack<string> strings = new Stack<string>(2);
+            strings.inn("Yes");
+            strings.inn("Ok");
+            strings.Clear();
+            Console.ReadKey();
+        }
+	}
+}
